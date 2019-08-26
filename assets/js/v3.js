ef9253d063842b652f74c19a22a59e0a6647d797
ramu12 <ramus@purplelogic.io>
2019-08-26 10:54:09 +0530

    added assets

---
+++ assets/js/v3.js
@@ -0,0 +1,205 @@
+(function($) {
+
+	"use strict";
+
+
+    /*------------------------------------------
+        = FUNCTIONS
+    -------------------------------------------*/
+    // Check ie and version
+    function isIE () {
+        var myNav = navigator.userAgent.toLowerCase();
+        return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1], 10) : false;
+    }
+
+
+
+    // Hero slider background setting
+    function sliderBgSetting() {
+        if ($(".hero-slider .slide").length) {
+            $(".hero-slider .slide").each(function() {
+                var $this = $(this);
+                var img = $this.find(".slider-bg").attr("src");
+
+                $this.css({
+                    backgroundImage: "url("+ img +")",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center center"
+                })
+            });
+        }
+    }
+sliderBgSetting();
+
+    //Setting hero slider
+    function heroSlider() {
+        if ($(".hero-slider").length) {
+            $(".hero-slider").slick({
+                autoplay: true,
+                autoplaySpeed: 6000,
+                pauseOnHover: true,
+                arrows: true,
+                prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+                nextArrow: '<button type="button" class="slick-next">Next</button>',
+                dots: true,
+                fade: true,
+                cssEase: 'linear'
+            });
+        }
+    }
+heroSlider();
+
+    /*------------------------------------------
+        = HIDE PRELOADER
+    -------------------------------------------*/
+    function preloader() {
+        if($('.preloader').length) {
+            $('.preloader').delay(100).fadeOut(500, function() {
+
+                //active wow
+                wow.init();
+
+                //Active heor slider
+                heroSlider();
+
+            });
+        }
+    }
+    preloader();
+
+
+    /*------------------------------------------
+        = FUNCTION FORM SORTING GALLERY
+    -------------------------------------------*/
+    function sortingGallery() {
+        if ($(".sortable-gallery .gallery-filters").length) {
+            var $container = $('.gallery-container');
+            $container.isotope({
+                filter:'*',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+            });
+
+            $(".gallery-filters li a").on("click", function() {
+                $('.gallery-filters li .current').removeClass('current');
+                $(this).addClass('current');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter:selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false,
+                    }
+                });
+                return false;
+            });
+        }
+    }
+
+    sortingGallery();  
+
+
+
+
+    /*------------------------------------------
+        = PARTNERS SLIDER
+    -------------------------------------------*/
+    if ($(".partners-slider").length) {
+        $(".partners-slider").owlCarousel({
+            autoplay:true,
+            smartSpeed: 300,
+            margin: 30,
+            loop:true,
+            autoplayHoverPause:true,
+            dots: false,
+            responsive: {
+                0 : {
+                    items: 2
+                },
+
+                400 : {
+                    items: 3
+                },
+
+                550 : {
+                    items: 4
+                },
+
+                992 : {
+                    items: 5
+                }
+            }
+        });
+    }
+
+     /*------------------------------------------
+        = FAN FACT COUNT
+    -------------------------------------------*/
+    if ($(".start-count").length) {
+        $('.counter').appear();
+        $(document.body).on('appear', '.counter', function(e) {
+            var $this = $(this),
+            countTo = $this.attr('data-count');
+
+            $({ countNum: $this.text()}).animate({
+                countNum: countTo
+            }, {
+                duration: 3000,
+                easing:'linear',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                }
+            });
+        });
+    }
+
+
+     /*------------------------------------------
+        = GOOGLE MAP
+    -------------------------------------------*/  
+    function map() {
+
+        var locations = [
+            ['Zopak It solutions ', 17.432195, 78.445911,1],
+            ['Zopak software private limited', 17.430745, 78.445611,2],
+        ];
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: new google.maps.LatLng( 17.432195, 78.445911),
+            zoom: 12,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) {  
+                marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                map: map,
+                icon:'assets/images/map-marker.png'
+            });
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+
+    }; 
+map();
+
+
+})(window.jQuery);
